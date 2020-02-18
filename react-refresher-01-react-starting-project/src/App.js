7c52ffcea7d627b5d69290021878d36228482997
nbayram <nbayram4@gmail.com>
2020-02-18 20:44:53 +0100

    Udemy Course Starts

---
+++ react-refresher-01-react-starting-project/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './App.css';
+import GoalList from './Components/GoalList';
+
+const App = () => {
+  const courseGoals = [
+    {
+      id: "cg1",
+      text: "Finish the Course"
+    },
+    {
+      id: "cg2",
+      text: "Learn all about the Course Main Topic"
+    },
+    {
+      id: "cg3",
+      text: "Help other students in the Course Q&amp;A"
+    }
+  ];
+  return (
+    <div className="course-goals" >
+      <h2>Course Goals</h2>
+      <GoalList goals={courseGoals} />
+    </div>
+  )
+};
+
+export default App;
