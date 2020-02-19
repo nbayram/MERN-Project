df2c4ec75a9c9d96b314ee5eb963994fba5c4f67
nbayram <nbayram4@gmail.com>
2020-02-19 14:38:22 +0100

    Fetching User Input

--- react-refresher-01-react-starting-project/src/Components/NewGoal/NewGoal.js
+++ react-refresher-01-react-starting-project/src/Components/NewGoal/NewGoal.js
@@ -1,22 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewGoal.css';
 
 export default function NewGoal(props) {
+  const [enteredText, setEnteredText] = useState("");
+
+  const textChangeHandler = event => {
+    setEnteredText(event.target.value);
+  }
 
   const addGoalHandler = event => {
-    event.preventDefault();
-
     const newGoal = {
       id: Math.random().toString(),
-      text: "My new goal!"
+      text: enteredText
     }
 
+    event.preventDefault();
     props.onAddGoal(newGoal);
+    setEnteredText('');
   }
 
   return (
     <form className="new-goal" onSubmit={addGoalHandler} >
-      <input type="text" />
+      <input type="text" value={enteredText} onChange={textChangeHandler} />
       <button type="submit" >Add Goal</button>
     </form>
   )
