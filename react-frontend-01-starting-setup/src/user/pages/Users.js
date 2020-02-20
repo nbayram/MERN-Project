fb374c5dc41967c7d1fcb0ba486d0e0c4e81eb8a
nbayram <nbayram4@gmail.com>
2020-02-20 23:17:25 +0100

    Adding a UserItem Component

--- react-frontend-01-starting-setup/src/user/pages/Users.js
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -7,7 +7,7 @@ const Users = () => {
     {
       id: 'u1',
       name: 'Nurullah Bayram',
-      image: 'https://source.unsplash.com/600x400/?cat%20%20%20%20%20%20%20&__cacheBuster=1582234309654',
+      image: 'https://www.jpl.nasa.gov/spaceimages/images/largesize/PIA23595_hires.jpg',
       places: 3
     }
   ];
