3420688e2e2d339a3578326e86f488e7e06e5b01
nbayram <nbayram4@gmail.com>
2020-02-20 23:04:28 +0100

    Adding a UsersList Page / Component

--- react-frontend-01-starting-setup/src/user/pages/Users.js
+++ react-frontend-01-starting-setup/src/user/pages/Users.js
@@ -1,5 +1,18 @@
-import React from 'react'
+import React from 'react';
+import UserList from '../components/UsersList';
 
-export default function Users() {
-  return <h2>Users work!</h2>
+const Users = () => {
+
+  const USERS = [
+    {
+      id: 'u1',
+      name: 'Nurullah Bayram',
+      image: 'https://source.unsplash.com/600x400/?cat%20%20%20%20%20%20%20&__cacheBuster=1582234309654',
+      places: 3
+    }
+  ];
+
+  return <UserList items={USERS} />
 }
+
+export default Users
