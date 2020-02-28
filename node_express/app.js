8587369505e73428b60607774884660b675a620e
nbayram <nbayram4@gmail.com>
2020-02-28 14:46:08 +0100

    Sending Requests & Responses

--- node_express/app.js
+++ node_express/app.js
@@ -1,11 +1,23 @@
-const fs = require('fs');
+const http = require('http');
 
-const userName = "Nuri";
+const server = http.createServer((req, res) => {
+  console.log('INCOMING REQUEST');
+  console.log(req.method, req.url);
 
-fs.writeFile('user-data.txt', 'Name:' + userName, (err) => {
-  if (err) {
-    console.log(err);
-    return;
+  if (req.method === "POST") {
+    let body = '';
+    req.on('end', () => {
+      const username = body.split('=')[1];
+      res.end('<h1>' + username + '</h1>');
+    });
+
+    req.on('data', (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>');
   }
-  console.log('WROTE FILE');
-})
+});
+
+server.listen(5000);
