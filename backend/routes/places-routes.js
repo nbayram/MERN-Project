a26d66b4646f21df66d812cde82a1478dcb35e86
nbayram <nbayram4@gmail.com>
2020-02-28 20:28:49 +0100

    Handling Error

--- backend/routes/places-routes.js
+++ backend/routes/places-routes.js
@@ -23,6 +23,12 @@ router.get('/:pid', (req, res, next) => {
     return p.id === placeId;
   });
 
+  if (!place) {
+    const error = new Error('Could not find a place for the provided id.');
+    error.code = 404;
+    throw error;
+  }
+
   res.json({ place }); // { place } => { place: place }
 });
 
@@ -30,8 +36,14 @@ router.get('/user/:uid', (req, res, next) => {
   const userId = req.params.uid;
 
   const place = DUMMY_PLACES.find(p => {
-    return p.creator = userId;
+    return p.creator === userId;
   });
+
+  if (!place) {
+    const error = new Error('Could not find a place for the provided user id.');
+    error.code = 404;
+    return next(error);
+  }
 
   res.json({ place });
 });
